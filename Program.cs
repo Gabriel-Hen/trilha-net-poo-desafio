a75b36983fb4f2ac657536db700aea5b7e66cc70
Gabriel Henrique <gabriel.hamazonas@gmail.com>
2025-06-25 00:39:51 -0300

    Extended Nokia.cs and Iphone.cs classes from Smartphone.cs and implemented logic for "InstalarAplicativo" method. Added new properties so they look like the diagram.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone modelo nokia:");
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia2", imei: "123", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+Console.WriteLine("Smartphone modelo Iphone:");
+
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Iphone2", imei: "123", memoria: 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
